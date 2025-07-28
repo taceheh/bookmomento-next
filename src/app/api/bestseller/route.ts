3ca6f620de5d4d85330a49194bfaec5b327cea9b
wonheeki <wonluvv229@gmail.com>
2025-07-28 21:15:18 +0900

    feat: 알라딘 베스트셀러 API 연동 및 응답 데이터 반환 구현

---
+++ src/app/api/bestseller/route.ts
@@ -0,0 +1,27 @@
+import { NextResponse } from 'next/server';
+import axios from 'axios';
+
+export async function GET() {
+  try {
+    const response = await axios.get(
+      'http://www.aladin.co.kr/ttb/api/ItemList.aspx',
+      {
+        params: {
+          ttbkey: process.env.NEXT_PUBLIC_ALADIN_API_KEY, // .env로 키 분리
+          QueryType: 'BestSeller',
+          version: '20131101',
+          SearchTarget: 'Book',
+          MaxResults: '5',
+          Output: 'JS',
+          Cover: 'Big',
+        },
+      },
+    );
+
+    const items = response.data.item;
+    return NextResponse.json(items);
+  } catch (error) {
+    console.error('알라딘 API 오류:', error);
+    return new NextResponse('Internal Server Error', { status: 500 });
+  }
+}
