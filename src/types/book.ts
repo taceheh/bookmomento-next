04007c6cf5b40d08b0f285097997544afd645ebf
wonheeki <wonluvv229@gmail.com>
2025-08-01 00:40:50 +0900

    refactor : Book type 생성 및 분리

---
+++ src/types/book.ts
@@ -0,0 +1,11 @@
+export interface Book {
+  author: string;
+  categoryName: string;
+  cover: string;
+  description: string;
+  isbn: string;
+  priceStandard: string;
+  pubDate: string;
+  publisher: string;
+  title: string;
+}
