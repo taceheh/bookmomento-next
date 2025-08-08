d8563967e3411073cd981f67c58ccb5c4370dc4c
wonheeki <wonluvv229@gmail.com>
2025-08-08 18:01:41 +0900

    feat : auth/callback 제거

--- src/app/auth/callback/route.ts
+++ src/app/auth/callback/route.ts
@@ -1,46 +1,46 @@
-import { supabase } from '@/lib/supabase';
-import { NextRequest, NextResponse } from 'next/server';
-import { PrismaClient } from '@prisma/client';
+// import { supabase } from '@/lib/supabase';
+// import { NextRequest, NextResponse } from 'next/server';
+// import { PrismaClient } from '@prisma/client';
 
-const prisma = new PrismaClient();
+// const prisma = new PrismaClient();
 
-export async function GET(req: NextRequest) {
-  const { searchParams } = new URL(req.url);
-  const code = searchParams.get('code');
-  const next = searchParams.get('next') ?? '/';
+// export async function GET(req: NextRequest) {
+//   const { searchParams } = new URL(req.url);
+//   const code = searchParams.get('code');
+//   const next = searchParams.get('next') ?? '/';
 
-  if (!code) {
-    return NextResponse.redirect(`/auth/auth-code-error`);
-  }
+//   if (!code) {
+//     return NextResponse.redirect(`/auth/auth-code-error`);
+//   }
 
-  const { data, error } = await supabase.auth.exchangeCodeForSession(code);
+//   const { data, error } = await supabase.auth.exchangeCodeForSession(code);
 
-  if (error || !data.session?.user) {
-    console.error('Supabase 인증 오류:', error);
-    return NextResponse.redirect(`/auth/auth-code-error`);
-  }
+//   if (error || !data.session?.user) {
+//     console.error('Supabase 인증 오류:', error);
+//     return NextResponse.redirect(`/auth/auth-code-error`);
+//   }
 
-  const user = data.session.user;
+//   const user = data.session.user;
 
-  try {
-    await prisma.users.upsert({
-      where: { id: user.id },
-      update: {
-        email: user.email,
-        nickname: user.user_metadata?.name ?? '',
-        provider: user.app_metadata?.provider,
-      },
-      create: {
-        id: user.id,
-        email: user.email,
-        nickname: user.user_metadata?.name ?? '',
-        provider: user.app_metadata?.provider,
-      },
-    });
-    console.log('Prisma upsert 완료');
-  } catch (e) {
-    console.error('Prisma upsert 실패:', e);
-  }
+//   try {
+//     await prisma.users.upsert({
+//       where: { id: user.id },
+//       update: {
+//         email: user.email,
+//         nickname: user.user_metadata?.name ?? '',
+//         provider: user.app_metadata?.provider,
+//       },
+//       create: {
+//         id: user.id,
+//         email: user.email,
+//         nickname: user.user_metadata?.name ?? '',
+//         provider: user.app_metadata?.provider,
+//       },
+//     });
+//     console.log('Prisma upsert 완료');
+//   } catch (e) {
+//     console.error('Prisma upsert 실패:', e);
+//   }
 
-  return NextResponse.redirect(next);
-}
+//   return NextResponse.redirect(next);
+// }
