123e114d7db3b46df36e88d1ab6c7d715dd9093f
wonheeki <wonluvv229@gmail.com>
2025-08-22 15:56:53 +0900

    feat : AuthHydrator 배치 - 전역 상태를 모두 하위 컴포넌트에서 즉시 사용할 수 있도록 구조 정리

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,20 @@
 import { Header } from '@/components/layout/header';
 import './globals.css';
 import { Footer } from '@/components/layout/footer';
+import { getUserServer } from '@/lib/auth/server';
+import AuthHydrator from '@/components/AuthHydrator';
 export const dynamic = 'force-dynamic';
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const user = await getUserServer();
   return (
     <html lang="en">
       <body>
         <div className="max-w-[650px] min-h-screen mx-auto bg-white ">
+          <AuthHydrator initialUser={user} />
           <Header />
           <main> {children}</main>
           <Footer />
