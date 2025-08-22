ff0057cc64a28b55ced74dba2744855132c50938
wonheeki <wonluvv229@gmail.com>
2025-08-22 16:29:48 +0900

    refactor : 마이페이지 라우트 로직 리팩토링 - supabaseServer 직접 호출 제거 후 getUserServer 유틸 함수 사용

--- src/app/mypage/page.tsx
+++ src/app/mypage/page.tsx
@@ -1,26 +1,9 @@
-// app/mypage/page.tsx
-// 절대 'use client' 금지 (서버 컴포넌트여야 함)
-import { redirect } from 'next/navigation';
-import { supabaseServer } from '@/lib/supabaseServer';
+import { getUserServer } from '@/lib/auth/server';
 
-// 쿠키 읽는 페이지가 정적으로 캐시되지 않도록 (권장)
 export const dynamic = 'force-dynamic';
 
 export default async function Page() {
-  const supabase = await supabaseServer(); // ← await 중요
-  const {
-    data: { session },
-    error,
-  } = await supabase.auth.getSession();
-  console.log(session);
-  const {
-    data: { user },
-  } = await supabase.auth.getUser();
-
-  if (!user) {
-    // 로그인 안된 경우 로그인 페이지로
-    redirect('/signin?next=%2Fmypage');
-  }
+  const user = await getUserServer();
   console.log(user);
 
   return <div>마이페이지 내용이 여기에 들어갑니다.</div>;
