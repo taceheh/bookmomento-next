594a861dd756b133b3fe8a25c2cb7e27804e9e3a
wonheeki <wonluvv229@gmail.com>
2025-08-22 17:22:47 +0900

    feat : 로그아웃 기능 추가 및 버튼 컴포넌트화

--- src/app/mypage/page.tsx
+++ src/app/mypage/page.tsx
@@ -1,10 +1,19 @@
-import { getUserServer } from '@/lib/auth/server';
+import { KakaoLogoutButton } from '@/components/KakaoLogoutButton';
+import { getUserServer, requireUserServer } from '@/lib/auth/server';
+import { supabase } from '@/lib/supabase';
+import { redirect } from 'next/navigation';
 
 export const dynamic = 'force-dynamic';
 
 export default async function Page() {
-  const user = await getUserServer();
+  // const user = await getUserServer();
+  const user = await requireUserServer('/mypage');
   console.log(user);
 
-  return <div>마이페이지 내용이 여기에 들어갑니다.</div>;
+  return (
+    <div>
+      <div>마이페이지 내용이 여기에 들어갑니다.</div>
+      <KakaoLogoutButton />
+    </div>
+  );
 }
