b061cbd2463681264f5b3ac26d4e3badc027f69c
wonheeki <wonluvv229@gmail.com>
2025-10-01 23:39:11 +0900

    fix : 댓글 user_id 타입 수정

--- src/app/book/[id]/page.tsx
+++ src/app/book/[id]/page.tsx
@@ -243,7 +243,7 @@ type CommentItemType = {
   id: string;
   parent_id: string | null;
   body: string;
-  user_id: string;
+  user_id: string | null;
   created_at: string;
   deleted_at: string | null;
 };
