c7f662ee39bf3c5a2058ad8011332132659f7202
wonheeki <wonluvv229@gmail.com>
2025-10-17 23:54:15 +0900

    chore : Supabase Service Role Key추가

--- src/lib/supabaseServer.ts
+++ src/lib/supabaseServer.ts
@@ -1,6 +1,7 @@
 // src/lib/supabaseServer.ts
 import { cookies } from 'next/headers';
 import { createServerClient } from '@supabase/ssr';
+import { createClient } from '@supabase/supabase-js';
 
 export async function supabaseServer() {
   const jar = await cookies(); // Next 15: async
@@ -28,3 +29,8 @@ export async function supabaseServer() {
     },
   );
 }
+
+export const supabaseAdmin = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.SUPABASE_SERVICE_ROLE_KEY!,
+);
